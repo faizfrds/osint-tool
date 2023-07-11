ccb816af88f627ab25028353ddaabcdf9239474a
Faiz Firdaus <faiz.firdaus.2004@gmail.com>
2023-07-12 00:04:52 +0700

    i

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import { useState } from "react";
 import { data } from "autoprefixer";
-import { Button } from "react-bootstrap";
 
 export default function Home() {
   const [ip, setIP] = useState("");
